1d954dd0fc36669b9826f41745f885ac5ced9b75
Karthika Nagaraj <karthikanagaraj2611@gmail.com>
2026-05-25 17:05:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@reactjs/react-plugin' // (or vue, etc., depending on your framework)
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Personal-Portfolio/', // 👈 ADD THIS LINE
   plugins: [react()],
-  base: '/Personal-Portfolio/',
 })
